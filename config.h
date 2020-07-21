68aef2cbb494ac6f69f82669dce9c3397b71fb71
Rad Cirskis <nad2000@gmail.com>
2020-07-21 17:00:29 +1200

    my settings

--- config.h
+++ config.h
@@ -5,6 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
+/* static char *font = "Fira
+   Code:size=12:antialias=true:autohint=true;Liberation "
+                    "Mono:pixelsize=12:antialias=true:autohint=true"; */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
